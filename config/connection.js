f915d9ee4bba488789f7afbe97d4a14249a0ef9b
Vic Scherman <vicscherman@gmail.com>
2020-10-28 21:37:29 -0400

    Added jaws DB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 const mysql = require ('mysql');
 
-var connection =mysql.createConnection({
-    host: "localhost",
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+    connection = mysql.createConnection({
     port: 3306,
+    host: "localhost",
     user: "root",
     password: "Amandaforever69",
     database: "burger_db"
-});
+  });
+
+};
 
 connection.connect(function(err){
     if (err){
